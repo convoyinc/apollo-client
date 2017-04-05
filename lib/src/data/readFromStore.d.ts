69284becc782d9196179368babd76220b55932fa
Ian MacLeod <ian@nevir.net>
2017-04-05 10:48:35 -0700

    'Compile'

--- lib/src/data/readFromStore.d.ts
+++ lib/src/data/readFromStore.d.ts
@@ -1,13 +1,15 @@
 import { StoreContext } from './diffAgainstStore';
+import { StoreFetchMiddleware } from './fetchMiddleware';
 import { SelectionSet, Document } from 'graphql';
 import { FragmentMap } from '../queries/getFromAST';
 import { NormalizedCache } from './store';
-export declare function readQueryFromStore({store, query, variables, returnPartialData, fragmentMap}: {
+export declare function readQueryFromStore({store, query, variables, returnPartialData, fragmentMap, fetchMiddleware}: {
     store: NormalizedCache;
     query: Document;
     variables?: Object;
     returnPartialData?: boolean;
     fragmentMap?: FragmentMap;
+    fetchMiddleware?: StoreFetchMiddleware;
 }): Object;
 export declare function readFragmentFromStore({store, fragment, rootId, variables, returnPartialData}: {
     store: NormalizedCache;
