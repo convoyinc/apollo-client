b9b495fa0c307f4f2addf90ede0acfc53211ba11
Andreas Bergenwall <andreas@extech.fi>
2017-05-11 15:42:08 +0300

    Temp release

--- release/data/replaceQueryResults.d.ts
+++
@@ -1,9 +0,0 @@
-import { Cache } from './storeUtils';
-import { ApolloReducerConfig } from '../store';
-import { DocumentNode } from 'graphql';
-export declare function replaceQueryResults(cache: Cache, {queryId, variables, document, newResult}: {
-    queryId: string;
-    variables: any;
-    document: DocumentNode;
-    newResult: Object;
-}, config: ApolloReducerConfig): Cache;
--- release/data/store.d.ts
+++
@@ -1,6 +0,0 @@
-import { ApolloAction } from '../actions';
-import { QueryStore } from '../queries/store';
-import { MutationStore } from '../mutations/store';
-import { ApolloReducerConfig } from '../store';
-import { Cache } from './storeUtils';
-export declare function data(previousState: Cache | undefined, action: ApolloAction, queries: QueryStore, mutations: MutationStore, config: ApolloReducerConfig): Cache;
